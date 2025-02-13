b35b52a7e19c76df5501d310a17a9bf7ad60d693
Dayani <dayanihewawasam@gmail.com>
2025-02-14 03:22:28 +0530

    Skills

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './Components/Navbar/navbar'
 import Intro from './Components/Intro/intro'
-import Skills from './Components/Skills/skills'
+import Skills from './Components/Skills/Skills'
 import Projects from './Components/Project/Project'
 import Contact from './Components/Contact/contact'
 import Footer from './Components/Footer/footer'
