cc544e3889d21ec551cac0ab903bd5aa4784f78e
Dayani <dayanihewawasam@gmail.com>
2026-08-01 00:04:50 +0530

    changed the field

--- my-app/src/Components/Intro/intro.jsx
+++ my-app/src/Components/Intro/intro.jsx
@@ -17,15 +17,15 @@ const Intro = () => {
           I'm <span className="introName">
             <TypingEffect text={["Dayani Hewawasam,"]} speed={100} eraseDelay={2000} />
           </span> 
-          <br /> Frontend Developer
+          <br /> Aspiring Data Scientist
         </span>
 
         <p className="introPara">
-          As a passionate Computer Engineering undergraduate at the University of Ruhuna, I am deeply <br />
-          enthusiastic about frontend development. I focus on creating intuitive, visually appealing user <br />
-          experiences by combining technical skills with creative design. Always eager to learn and explore <br />
-          new technologies, I aim to contribute innovative solutions and work effectively within development teams.
-        </p>
+        As a passionate final-year Computer Engineering undergraduate at the University of Ruhuna, I am <br />
+deeply enthusiastic about Data Science, Machine Learning, and Artificial Intelligence. I enjoy <br />
+transforming data into meaningful insights and building intelligent systems that address real-world <br />
+challenges. Committed to continuous learning and innovation, I strive to contribute impactful <br />
+solutions while collaborating effectively within diverse teams. </p>
 
         <Link to="contact" smooth={true} duration={500}>
   <button className="btn">
